c5912a6b397eff8ee8a46a19f95b5ab0c161e2aa
Akira Yamamoto <devmonster2417@gmail.com>
2022-10-11 09:39:11 +0900

    hotfix: change main logic of jobs.ts

---
+++ src/data/jobs.ts
@@ -0,0 +1,35 @@
+const jobs = {
+  戦士: '戦',
+  剣士: '剣',
+  盗賊: '盗',
+  僧侶: '僧',
+  魔法使い: '魔',
+  狩人: '狩',
+  修道者: '修',
+  侍: '侍',
+  剣聖: '聖',
+  秘法剣士: '秘',
+  賢者: '賢',
+  忍者: '忍',
+  君主: '君',
+  ロイヤルライン: 'ロ',
+} as const;
+
+export const jobIcons = {
+  戦士: '01-001-fighter-01.png',
+  剣士: '01-002-swordsman-01.png',
+  盗賊: '01-003-thief-01.png',
+  僧侶: '01-004-priest-01.png',
+  魔法使い: '01-005-wizard-01.png',
+  狩人: '01-006-hunter-01.png',
+  修道者: '01-007-monk-01.png',
+  侍: '01-008-samurai-01.png',
+  剣聖: '01-009-swordsmaster-01.png',
+  秘法剣士: '01-010-magicfighter-01.png',
+  賢者: '01-011-sage-01.png',
+  忍者: '01-012-ninja-01.png',
+  君主: '01-013-king-01.png',
+  ロイヤルライン: '01-014-royalline-01.png',
+};
+
+export default jobs;
